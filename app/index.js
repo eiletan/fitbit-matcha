12c1bff3b54c3ae32c70a4ebf56ffc3cabeb0a7d
eiletan <nathanfhlai@gmail.com>
2024-05-29 21:56:52 -0700

    Complete clockface

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import * as document from "document";
 import { preferences } from "user-settings";
 import { me as appbit } from "appbit";
 import { HeartRateSensor } from "heart-rate";
+import { today } from "user-activity";
+import { battery } from "power";
 
 function zeroPad(i) {
   if (i < 10) {
@@ -17,11 +19,29 @@ clock.granularity = "minutes";
 // Get a handle on the <text> element
 const myLabel = document.getElementById("myLabel");
 
+// Get a handle on the <text> element which is used to display date
+const theDate = document.getElementById("date");
+
 // Get a handle for the <image> element which is used as the background image
-const bg = document.getElementById("background-img");
+const bg = document.getElementById("backgroundImg");
+
+// Get a handle for the <image> element which is used as the battery icon
+const batteryIcon = document.getElementById("batteryImg");
 
 // Get a handle for the <text> element which is used to display heart rate
 const heartRate = document.getElementById("heartRate");
+
+// Get a handle for the <text> element which is used to display step count
+const stepCount = document.getElementById("stepCount");
+
+// Get a handle for the <text> element which is used to display step count
+const calories = document.getElementById("calories");
+
+// Get a handle for the <text> element which is used to display battery level
+const batteryLevel = document.getElementById("batteryPercent");
+
+
+
 
 // Time ranges for images
 let morning = [6, 11];
@@ -34,6 +54,15 @@ let shibuya ="shibuya-crossing.jpg";
 let fuji = "mt-fuji.jpg";
 let ninenzaka ="ninenzaka.jpg";
 let shinkansen = "bullet-train-mt-fuji.jpg";
+let batteryFull = "battery-full.png";
+let battery75 = "battery-seventyfive.png";
+let batteryHalf = "battery-half.png";
+let battery25 = "battery-twentyfive.png";
+
+// Days array
+let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+// Months array
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 function dynamicImageSwitch(hour) {
   if ((hour >= morning[0] && hour <= morning[1])) {
@@ -55,6 +84,25 @@ function dynamicImageSwitch(hour) {
   }
 }
 
+function dynamicBatteryImg () {
+  if (battery.chargeLevel > 75) {
+    batteryIcon.href = batteryFull;
+  } else if (battery.chargeLevel > 50) {
+    batteryIcon.href = battery75;
+  } else if (battery.chargeLevel > 25) {
+    batteryIcon.href = batteryHalf;
+  } else {
+    batteryIcon.href = battery25;
+  }
+}
+
+function updateStepsAndCalories() {
+  if (appbit.permissions.granted("access_activity")) {
+    stepCount.text = today.adjusted.steps;
+    calories.text = today.adjusted.calories;
+  }
+}
+
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
   let today = evt.date;
@@ -70,12 +118,26 @@ clock.ontick = (evt) => {
   let mins = zeroPad(today.getMinutes());
   dynamicImageSwitch(hours24);
   myLabel.text = `${hours}:${mins}`;
+  theDate.text = `${days[today.getDay()]}, ${months[today.getMonth()]} ${today.getDate()}`;
+  updateStepsAndCalories();
 }
 
 if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
   const hrm = new HeartRateSensor({ frequency: 1 });
   hrm.addEventListener("reading", () => {
-    heartRate.text = `\u2665${hrm.heartRate}`;
+    heartRate.text = `${hrm.heartRate}`;
   });
   hrm.start();
+}
+
+
+
+battery.onchange = (charger, evt) => {
+  if (battery.chargeLevel == 100) {
+    batteryLevel.text = battery.chargeLevel;  
+  } else {
+    batteryLevel.text = battery.chargeLevel + "%";
+  }
+  
+  dynamicBatteryImg();
 }
